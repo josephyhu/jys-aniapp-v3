8e41b60e13b84a9d0f122ce3e4f62e17a0895342
Joseph Yhu <josephsyhu@gmail.com>
2022-06-28 17:13:07 -0400

    Testing...

--- inc/functions.php
+++ inc/functions.php
@@ -142,8 +142,8 @@ function get_userMangaList($userId, $status) {
 // Enable search functionality.
 function search_media($type, $search) {
     $query = 'query ($type: MediaType, $search: String) {
-        MediaList (type: $type, sort: SCORE_DESC)  {
-            media (search: $search) {
+        MediaList {
+            media (type: $type, search: $search, sort: SCORE_DESC) {
                 id,
                 coverImage {
                     medium,
