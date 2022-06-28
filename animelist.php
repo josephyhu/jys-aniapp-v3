390c02b18dbe361709c916e102617a3fda03c9ed
Joseph Yhu <josephsyhu@gmail.com>
2022-06-28 16:14:45 -0400

    Remove the vardump.

--- animelist.php
+++ animelist.php
@@ -41,7 +41,6 @@ require_once 'inc/header.php'; ?>
 
     if (!empty($data)) {
         if ($status === 'CURRENT') {
-            var_dump($data);
             echo '<h3>Currently Watching</h3>';
             echo '<table>';
             echo '<tbody>';
