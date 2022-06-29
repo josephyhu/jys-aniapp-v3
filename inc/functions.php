7455021fe7552606c0ebf4d0b923039c47e7318a
Joseph Yhu <josephsyhu@gmail.com>
2022-06-29 00:17:23 -0400

    Update anime details page.

--- inc/functions.php
+++ inc/functions.php
@@ -190,6 +190,12 @@ function get_animeDetails($id) {
                 month,
                 day,
             },
+            season,
+            seasonYear,
+            episodes,
+            duration,
+            genre,
+            source,
         }
     }';
 
