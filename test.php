0815fb02aa111aad17152e7187c3c78f967f3d5d
Joseph Yhu <josephsyhu@gmail.com>
2022-06-29 16:15:51 -0400

    testing

--- test.php
+++ test.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
 require_once 'inc/functions.php';
-$data = test($_SESSION['accessToken'], 9253, 9);
+try {
+    $data = test($_SESSION['accessToken'], 9253, 9);
+} catch (Exception $e) {
+    $e->getMessage();
+}
 var_dump($data);
