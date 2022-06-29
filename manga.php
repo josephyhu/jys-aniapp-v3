83e6ef830494c46baa30373cceca5e0421a09d30
Joseph Yhu <josephsyhu@gmail.com>
2022-06-29 07:24:30 -0400

    Show add media link only when media is accessed from search.

--- manga.php
+++ manga.php
@@ -75,7 +75,7 @@ require_once 'inc/header.php';
             echo "</div>";
             echo "</div>";
 
-            if (isset($_SESSION['userId'])) {
+            if (isset($_SESSION['userId']) && !isset($userId)) {
             ?>
                 <a href='add_manga.php?id=<?php echo $id; ?>'>Add manga</a>
             <?php
