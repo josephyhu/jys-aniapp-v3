3cdd56767211974937e6b589abfc3fbef4daa63e
Joseph Yhu <josephsyhu@gmail.com>
2022-06-30 23:26:31 -0400

    fix

--- index.php
+++ index.php
@@ -17,10 +17,12 @@ require_once 'inc/header.php';
 ?>
 <main>
     <?php
-        if (isset($code) || $logged_in == 1) {
+        if (isset($code)) {
             $_SESSION['accessToken'] = get_token($code);
             $_SESSION['userId'] = get_userId($_SESSION['accessToken']);
             $_SESSION['username'] = get_username($_SESSION['userId']);
+        }
+        if ($logged_in == 1) {
             echo "<div class='links'><a href='index.php?logged_in=1'>Home</a>";
             echo "<a href='animelist.php'>Anime List</a>";
             echo "<a href='mangalist.php'>Manga List</a>";
