0a2963f34d084142923f65eda3853961aa60d4e2
Joseph Yhu <josephsyhu@gmail.com>
2022-07-05 20:20:18 -0400

    testing

--- delete_anime.php
+++ delete_anime.php
@@ -7,16 +7,11 @@ $userId = $_GET['userId'];
 
 require_once 'inc/header.php';
 
-$data = delete_userAnime($_SESSION['accessToken'], $id);
-
-var_dump($data);
-
-if (delete_userAnime($_SESSION['accessToken'], $id)) {
-            echo "<p class='success'>Anime successfully deleted.</p>";
-            sleep(3);
-            header('Location: animelist.php');
-        } else {
-            echo "<p class='warning'>Deleting anime failed.</p>";
-            sleep(3);
-            header('Location: anime.php?userId="' . $userId . '&id=' . $id .  '"');
-        }
+try {
+    delete_userAnime($_SESSION['accessToken'], $id);
+    echo "<p class='success'>Anime successfully deleted.</p>";
+    sleep(3);
+    header('Location: animelist.php');
+} catch (Exception $e) {
+    echo $e->getMessage;
+}
