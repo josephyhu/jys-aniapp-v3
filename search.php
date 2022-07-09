e13368bb572a95dc688c298221285ef282db5610
Joseph Yhu <josephsyhu@gmail.com>
2022-07-08 21:45:50 -0400

    testing something

--- search.php
+++ search.php
@@ -20,7 +20,7 @@ require_once 'inc/header.php';
     <?php
     try {
         $users = test();
-        echo max($users['statistics']['anime']['count']);
+        var_dump($users['statistics']['anime']['count']);
     } catch (Exception $e) {
         echo $e->getMessage();
     }
